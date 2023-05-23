c6af64a97d628f2c253e4e6d84d021ab60eaa0f9
Max Howell <mxcl@me.com>
2023-05-23 10:40:17 -0400

    Don’t use tea installed git at this time (#6)
    Fixes bug where git would fail because full env was not hydrated

--- src/hooks/useSync.ts
+++ src/hooks/useSync.ts
@@ -3,9 +3,6 @@
 import { writeAll } from "deno/streams/write_all.ts"
 import { flock } from "../utils/flock.deno.ts"
 import useDownload from "./useDownload.ts"
-import useShellEnv from "./useShellEnv.ts"
-import { Installation } from "../types.ts"
-import SemVer from "../utils/semver.ts"
 import usePantry from "./usePantry.ts"
 import useConfig from "./useConfig.ts"
 import Path from "../utils/Path.ts"
@@ -69,24 +66,13 @@ export default async function(logger?: Logger) {
 //////////////////////// utils
 
 async function git(...args: (string | Path)[]) {
-  const { git, prefix } = useConfig()
-  const env = await (async () => {
-    //TODO lol hacky
-    if (!git?.string.startsWith(prefix.string)) return
-    const installation: Installation = {
-      path: git.parent().parent(),
-      pkg: { project: 'git-scm.org', version: new SemVer('2.40.0')}
-    }
-    const { map, flatten } = useShellEnv()
-    return flatten(await map({installations: [installation]}))
-  })()
+  const { git } = useConfig()
   if (!git) throw new Error("no-git")  // caught above to trigger http download instead
-  await run({cmd: [git, ...args], env})
+  await run({cmd: [git, ...args]})
 }
 
 export interface RunOptions {
   cmd: (string | Path)[]
-  env?: Record<string, string>
 }
 
 async function run(opts: RunOptions) {
