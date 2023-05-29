1410136276e76f9eab95b3d14bfad0a154928b24
Max Howell <mxcl@me.com>
2023-05-29 09:00:20 -0400

    wip

--- src/prefab/run.ts
+++ src/prefab/run.ts
@@ -5,18 +5,13 @@ import * as semver from "../utils/semver.ts"
 import useSync from "../hooks/useSync.ts"
 import Path from "../utils/Path.ts"
 import hydrate from "./hydrate.ts"
-import { isString } from 'is-what'
 import resolve from "./resolve.ts"
 import install from "./install.ts"
 import link from "./link.ts"
 
-export async function spawn(cmd: string | string[] | Path[], opts: SpawnOptions) {
+export async function spawn(cmd: string, args: (string | Path)[] = [], opts?: SpawnOptions) {
   const pantry = usePantry()
   const sh = useShellEnv()
-
-  cmd = isString(cmd) ? cmd.split(' ') : cmd.map(x => `${x}`)
-  if (cmd.length === 0) throw new RunError('EUSAGE', '`cmd` cannot be empty')
-  const arg0 = cmd[0]
 
   if (pantry.needsUpdate()) {
     useSync()
@@ -26,11 +21,11 @@ export async function spawn(cmd: string | string[] | Path[], opts: SpawnOptions)
     for await (const { project } of pantry.ls()) {
       const provides = await pantry.project(project).provides()
       //TODO handle eg. node^16 here
-      if (provides.includes(arg0)) {
+      if (provides.includes(cmd)) {
         return project
       }
     }
-    throw new RunError('ENOENT', `No project in pantry provides ${arg0}`)
+    throw new RunError('ENOENT', `No project in pantry provides ${cmd}`)
   })()
 
   const { pkgs } = await hydrate({ project, constraint: new semver.Range('*') })
@@ -43,7 +38,7 @@ export async function spawn(cmd: string | string[] | Path[], opts: SpawnOptions)
 
   const env = await sh.map({ installations: installed })
 
-  for (const [key, value] of Object.entries(opts.env ?? {})) {
+  if (opts?.env) for (const [key, value] of Object.entries(opts.env)) {
     if (!value) {
       continue
     } else if (env[key]) {
@@ -53,9 +48,10 @@ export async function spawn(cmd: string | string[] | Path[], opts: SpawnOptions)
     }
   }
 
+  opts ??= {}
   opts.env = sh.flatten(env)
 
-  return node_spawn(cmd[0], cmd.slice(1), opts)
+  return node_spawn(cmd[0], args.map(x=>x.toString()), opts)
 }
 
 export default { spawn }
