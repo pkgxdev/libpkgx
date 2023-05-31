8ba02fbe6e0ef0e73d893a5456d8958a5684642e
Max Howell <mxcl@me.com>
2023-05-31 10:56:35 -0400

    wip

--- src/porcelain/run.test.ts
+++ src/porcelain/run.test.ts
@@ -1,5 +1,5 @@
-import { useTestConfig } from "../hooks/useTestConfig.ts"
 import { assertEquals, assertRejects } from "deno/testing/asserts.ts"
+import { useTestConfig } from "../hooks/useTestConfig.ts"
 import run from "./run.ts"
 
 Deno.test("porcelain.runx", async runner => {
@@ -25,18 +25,18 @@ Deno.test("porcelain.runx", async runner => {
   await runner.step(async function stdout() {
     useTestConfig()
     const { stdout } = await run(['python', '-c', "import os; os.getenv('FOO')"], { stdout: true, env: { FOO: "FOO" } })
-    assertEquals(stdout, "FOO")
+    assertEquals(stdout, "FOO\n")
   })
 
   await runner.step(async function stderr() {
     useTestConfig()
     const { stderr } = await run(['node', '-e', "console.error(process.env.FOO)"], { stderr: true, env: { FOO: "BAR" } })
-    assertEquals(stderr, "BAR")
+    assertEquals(stderr, "BAR\n")
   })
 
-  await runner.step(async function captureAll() {
+  await runner.step(async function all() {
     useTestConfig()
-    const { stderr, stdout, status } = await run(['node', '-e', "console.error(1); console.log(2); process.exit(3"], { stderr: true, stdout: true, status: true })
+    const { stderr, stdout, status } = await run(['node', '-e', "console.error(1); console.log(2); process.exit(3)"], { stderr: true, stdout: true, status: true })
     assertEquals(stderr, "1\n")
     assertEquals(stdout, "2\n")
     assertEquals(status, 3)
