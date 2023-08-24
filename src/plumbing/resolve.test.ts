7d314bef5b002892b4bebb3972720965313b8c53
Max Howell <mxcl@me.com>
2023-08-24 09:52:02 -0400

    wip

--- src/plumbing/resolve.test.ts
+++ src/plumbing/resolve.test.ts
@@ -11,7 +11,7 @@ import SemVer from "../utils/semver.ts"
 import Path from "../utils/Path.ts"
 
 Deno.test("resolve cellar.has", {
-  permissions: {'read': true, 'env': ["TMPDIR", "HOME"], 'write': [Deno.env.get("TMPDIR") ?? "/tmp"] }
+  permissions: {'read': true, 'env': ["TMPDIR", "TMP", "HOME"], 'write': [Deno.env.get("TMPDIR") ?? Deno.env.get("TMP") ?? "/tmp"] }
 }, async runner => {
   const prefix = useTestConfig().prefix
   const pkg = { project: "foo", version: new SemVer("1.0.0") }
