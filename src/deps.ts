6075ded25c498bee3f2350ec905e636de69d9a1c
Max Howell <mxcl@me.com>
2025-01-16 16:03:36 -0500

    Fix needing `deno.json`

--- src/deps.ts
+++ src/deps.ts
@@ -1,10 +1,10 @@
-import * as is_what from "is-what"
+import * as is_what from "https://deno.land/x/is_what@v4.1.15/src/index.ts"
 export { is_what }
 
-import { type PlainObject } from "is-what"
+import { type PlainObject } from "https://deno.land/x/is_what@v4.1.15/src/index.ts"
 export type { PlainObject }
 
-import * as outdent from "outdent"
+import * as outdent from "https://deno.land/x/outdent@v0.8.0/mod.ts"
 export { outdent }
 
 // importing super specifically to reduce final npm bundle size
